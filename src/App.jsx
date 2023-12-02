0cf28016b32b2ce97e3de725867e70abbe065c9c
0xrly <orlygiquez@gmail.com>
2023-12-02 15:48:44 -0500

    'starting'

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
+import { useState } from "react";
+
 const TURNS = {
   X: "x",
   O: "o",
 };
-
-const board = Array(9).fill(null);
 
 const Square = ({ children, updateBoard, index }) => {
   return <div className="square">{children}</div>;
 };
 
 const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <section className="game">
         {board.map((_, index) => {
-          return (
-            <Square updateBoard={null} index={index}>
-              {index}
-            </Square>
-          );
+          return <Square key={index} index={index}></Square>;
         })}
       </section>
     </main>
